e67ed29735ead99801766346d0847b2276c54d37
davidaqc <davidqc05@gmail.com>
2020-07-12 13:52:44 -0600

    ddd

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
-#include <game.hpp>
+//#include <game.hpp>
 
 using namespace std;
 
 int main()
 {
-    game *g1 = new game();
-    g1->run_game();
+    //game *g1 = new game();
+    //g1->run_game();
 
     return 0;
 }
